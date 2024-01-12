a5be5c4e813a4046bd145a080fe4091833c89d76
Agustin Hahn <agustin.f.hahn@gmail.com>
2024-01-12 20:50:40 -0300

    cambios logica slice

--- src/features/itSlice.js
+++ src/features/itSlice.js
@@ -1,30 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit'
-import stockArticulos from '../data/stock.json'
-import tareas from '../data/trabajos.json'
 
 const initialState = {
     value: {
-        products : stockArticulos,
-        tareasPendientes : tareas,
-        tareasFinalizadas : []
+        products: [],
+        tareasPendientes: [],
+        tareasFinalizadas: []
     }
 }
 
 export const itSlice = createSlice({
-    name:"it",
+
+    name: "it",
     initialState,
-    reducers:{
-        equipoUsado : (state, action) =>{
-            const {id} = action.payload;
-            const articulo = state.value.products.find((p) => p.id == id)
-            if(articulo){
-                articulo.cantidad -=1
-            }
+    reducers: {
+        setProducts: (state, action) => {
+            state.value.products = action.payload
+            console.log(state.value.products)
         },
-        estadoTarea : (state, action) =>{
-            const {idTarea} = action.payload;
+        equipoUsado: (state, action) => {
+            const { id } = action.payload;
+            const updateStock = state.value.products.map((p) => {
+                if(p.id == id){
+                    return{
+                        ...p,
+                        cantidad:p.cantidad -1
+                    }
+                }
+                return p
+            })
+            state.value.products = updateStock
+        },
+        estadoTarea: (state, action) => {
+            const { idTarea } = action.payload;
             const tareaIndex = state.value.tareasPendientes.findIndex((p) => p.id == idTarea);
-            if(tareaIndex !== -1){
+            if (tareaIndex !== -1) {
                 const tarea = state.value.tareasPendientes.splice(tareaIndex, 1)[0]
                 state.value.tareasFinalizadas.push(tarea)
             }
@@ -32,6 +41,6 @@ export const itSlice = createSlice({
     }
 })
 
-export const {equipoUsado, estadoTarea} = itSlice.actions
+export const { equipoUsado, estadoTarea, setProducts } = itSlice.actions
 
 export default itSlice.reducer
--- src/screens/Stock.js
+++ src/screens/Stock.js
@@ -1,18 +1,27 @@
 import { View, Text, FlatList } from 'react-native'
 import StockItem from '../components/StockItem'
 import { useGetStockQuery } from '../app/services/itServices'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import {setProducts } from '../features/itSlice'
 
 
 const Stock = () => {
+    const dispatch = useDispatch();
+    const { data: stockEquipos } = useGetStockQuery()
 
-    const {data: stockEquipos} = useGetStockQuery()
+    useEffect(() => {
+        if (stockEquipos) {
+            dispatch(setProducts(stockEquipos));
+        }
+    }, [stockEquipos, dispatch]);
 
     return (
         <>
-            <FlatList 
+            <FlatList
                 data={stockEquipos}
                 keyExtractor={item => item.id}
-                renderItem={({item}) => <StockItem item={item}/>}
+                renderItem={({ item }) => <StockItem item={item} />}
             />
         </>
     )
